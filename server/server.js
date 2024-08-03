65609aaba738588188664fdf8c63e92d1c622042
chilejay7 <codyburk7@yahoo.com>
2024-08-03 17:13:21 -0600

    server.js updated

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 7075;
 const path = require("path");
 const morgan = require("morgan");
 
-const routes = require('./routes');
+const routes = require("./routes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,6 +13,8 @@ app.use(morgan("dev"));
 app.get("/", (req, res) => {
     res.send("The server is up and running.  You are connected!")
 });
+
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log(`API server is running on PORT ${PORT}.`)
