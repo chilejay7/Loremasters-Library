fbbb9c4bd0b88e856605d82bebcba75eada485c7
chilejay7 <codyburk7@yahoo.com>
2024-08-05 18:30:05 -0600

    footer component created

---
+++ client/src/Components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer () {
+    return (
+        <footer>
+            <h4>chilejay7 Developer</h4>
+            <p>&#169; 2024</p>
+        </footer>
+    )
+}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Navbar from './Components/Navbar/Navbar.jsx'
+import Footer from './Components/Footer/Footer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
     <App />
+    <Footer />
   </React.StrictMode>,
 )
