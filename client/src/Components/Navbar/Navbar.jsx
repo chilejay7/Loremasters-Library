63ba6d7149e1d2e60ceba5bb0cee61f29e1ac84d
chilejay7 <codyburk7@yahoo.com>
2024-08-05 22:57:04 -0600

    font added

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,8 @@ import './Navbar.css'
 export default function Navbar() {
   return (
     <header id="main-header">
-      <h1>The Loremaster's Library</h1>
+      <h1 id="site-header">The Loremaster's Library</h1>
+      <p>A study of books & whiskey</p>
     </header>
   );
 }
