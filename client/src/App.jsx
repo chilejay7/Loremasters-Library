3eaed266bba4ab93b9e7549f64e41c95b4264302
chilejay7 <codyburk7@yahoo.com>
2024-08-07 10:05:32 -0600

    component renders for navbar and footer moved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Components/Home/Home";
+import Footer from "./Components/Footer/Footer";
 
 export default function App() {
-
-
   return (
-    <Home />
-
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
   );
 }
