04ade7b22696ea866aed09f89ab6336042ae3988
chilejay7 <codyburk7@yahoo.com>
2024-08-12 12:29:39 -0600

    book data passed to cards as prop

--- client/src/Components/Cards/BookCard.jsx
+++ client/src/Components/Cards/BookCard.jsx
@@ -1,19 +1,31 @@
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 
-export default function BookCard ({ book }) {
-
-  console.log('The card book data is:', book);
+export default function BookCard({ book }) {
 
   return (
-    <Card style={{ width: '18rem' }} key={book.id}>
+    <Card style={{ width: "18rem" }} key={book.id}>
       <Card.Body>
         <Card.Title>{book.volumeInfo.title}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-        <Card.Text>
-          Some quick example text to build on the card.
-        </Card.Text>
+        <Card.Subtitle className="mb-2 text-muted">
+          {book.volumeInfo.subtitle}
+        </Card.Subtitle>
+
+    <hr></hr>
+    <Card.Link href={book.volumeInfo.previewLink} target="_blank">
+        <img
+          src={
+            book.volumeInfo.imageLinks
+              ? book.volumeInfo.imageLinks.thumbnail
+              : ""
+          }
+          title={book.volumeInfo.title}
+          alt={`${book.volumeInfo.title} cover`}
+        />
+</Card.Link>
+    <hr></hr>
+        <Card.Text>{book.volumeInfo.description}</Card.Text>
         <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
+        
       </Card.Body>
     </Card>
   );
