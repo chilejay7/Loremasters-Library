02a9e13bca63c88400aaf7717ea3ec743fc64aee
chilejay7 <codyburk7@yahoo.com>
2024-08-16 12:59:58 -0600

    footer header format updated

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import './Footer.css'
 export default function Footer () {
     return (
         <footer id="footer">
-            <h4>&#169; Chilejay7</h4>
-            <p>Developer 2024</p>
+            <h4>&#169; Chilejay7 &#183; Developer 2024</h4>
+            <p></p>
         </footer>
     )
 }
