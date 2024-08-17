33baf04119fa073f8f4e2b43d6d627459a35a412
chilejay7 <codyburk7@yahoo.com>
2024-08-17 15:23:58 -0600

    pipe and cigar component created

--- client/src/Components/PipeCigar/PipeCigar.jsx
+++ client/src/Components/PipeCigar/PipeCigar.jsx
@@ -1,5 +1,5 @@
 const PipeCigar = () => {
-    return <h3 className="loading construction">Site Under Construction</h3>
+    return <h3 className="loading construction">Pipe and Cigar Lounge Under Construction</h3>
 }
 
 export default PipeCigar;
