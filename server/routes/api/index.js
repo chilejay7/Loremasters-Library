f99ce8af9daa1e4df2e45c0b6b7264ca2f2b0636
chilejay7 <codyburk7@yahoo.com>
2024-09-09 08:14:27 -0600

    login module updated on server

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 
+
 router.get("/", (req, res) => {
     res.send("API routes are responding!")
 });
+
+
 
 module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,13 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
+const login = require('./login.js');
 
-router.use("/api", apiRoutes);
+router.get("/", (req, res) => {
+    res.send("API routes are responding!")
+});
+
+router.use('/login', login);
+
+router.use('/api', apiRoutes);
 
 module.exports = router;
