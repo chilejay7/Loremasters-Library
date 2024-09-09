1b5eeb3b0aac47033cb34e56b6ceb67dc5aaeb51
chilejay7 <codyburk7@yahoo.com>
2024-09-09 08:18:21 -0600

    login / api config updated

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,9 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
-const login = require('./login.js');
 
 router.get("/", (req, res) => {
     res.send("API routes are responding!")
 });
-
-router.use('/login', login);
 
 router.use('/api', apiRoutes);
 
