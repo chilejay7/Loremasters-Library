4406018ccebcdd4546cefd3c8545cb28a5c56a8d
chilejay7 <codyburk7@yahoo.com>
2024-10-06 13:52:33 -0600

    fixes error in api target address

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'https://localhost: 7075',
+        target: 'https://localhost:7075',
         secure: false,
         changeOrigin: true
       }
