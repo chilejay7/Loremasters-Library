2598155533d9ec0e28aff3fcaaf0fd79b94fa7c6
chilejay7 <codyburk7@yahoo.com>
2024-10-14 15:23:44 -0600

    getUser query created

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,9 +1,12 @@
 const { User } = require('../models');
 
 const getUser = async (req, res) => {
-    const { user, username } = req.params;
+    const { user, username, id } = req.params;
     const searchedUser = await User.findOne({
-        $or: [{ _id: user ? user._id : params.id}, { username: username }],
+        $or: [
+            { _id: user ? user._id : id}, 
+            { username: username }
+        ],
     })
 
     !searchedUser ? res.status(400).json({ message: 'Cannot find the requested user.'})
