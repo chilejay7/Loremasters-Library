37e81847161d50ce3c1fb9683f4d9fbb28dccffa
chilejay7 <codyburk7@yahoo.com>
2024-10-28 17:17:49 -0600

    auth.js created on server

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,6 +4,24 @@ const expiration = '4h';
 
 const authMiddleware = (req, res, next) => {
     let token = req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+        token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+        return res.status(400).json({ message: 'No auth token associated with your current session.' });
+    }
+
+    try {
+        const { data } = jwt.verify(token, secret, { maxAge: expiration });
+        req.user = data;
+    } catch {
+        console.log('Invalid token');
+        return res.status(400).json({ message: 'invalid token' });
+    }
+
+    next();
 }
 
 
