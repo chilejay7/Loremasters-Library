1da53c990b1e06179336bc571fd4880e407bf5fa
chilejay7 <codyburk7@yahoo.com>
2025-04-28 15:41:26 -0600

    created new sequelize instance in connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -12,10 +12,21 @@
 
 // module.exports = mongoose.connection;
 
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const connection = new Sequelize({
-    dialect: 'mysql',
-    host: 'localhost',
-    port: 3306,
-})
+require("dotenv").config();
+
+process.env.JAWSDB_URL
+  ? (sequelize = new Sequelize(process.env.JAWSDB_URL))
+  : (sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        dialect: "mysql",
+        host: "localhost",
+        port: 3306,
+      }
+    ));
+
+module.exports = sequelize;
