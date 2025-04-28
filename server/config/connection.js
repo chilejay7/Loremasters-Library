7dcdb2f168c53c22a9cba4ff462d731e4b73a786
chilejay7 <codyburk7@yahoo.com>
2025-04-28 15:33:58 -0600

    created new sequelize instance in connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,21 @@
-const mongoose =  require('mongoose');
-const db = 'loremasters_library';
+// const mongoose =  require('mongoose');
+// const db = 'loremasters_library';
 
-mongoose
-    .connect(process.env.MONGODB_URI || `mongodb://127.0.0.1:27017/${db}`)
-    .then(() => {
-        console.log(`The loremaster's database is up and running`);
-    })
-    .catch((err) => {
-        console.error(err);
-    })
+// mongoose
+//     .connect(process.env.MONGODB_URI || `mongodb://127.0.0.1:27017/${db}`)
+//     .then(() => {
+//         console.log(`The loremaster's database is up and running`);
+//     })
+//     .catch((err) => {
+//         console.error(err);
+//     })
 
-module.exports = mongoose.connection;
+// module.exports = mongoose.connection;
+
+const Sequelize = require('sequelize');
+
+const connection = new Sequelize({
+    dialect: 'mysql',
+    host: 'localhost',
+    port: 3306,
+})
